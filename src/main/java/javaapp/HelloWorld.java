0691e45c981f2b7a65f6289ff83dd3d99f537861
KumarVeenja <157559044+KumarVeenja@users.noreply.github.com>
2024-08-14 23:49:10 -0400

    Initial commit

---
+++ src/main/java/javaapp/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaapp;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World Program for Jenkins");
+		System.out.println("Testing");
+		System.out.println("Testing again");
+		System.out.println("Testing again bnnbnbnb");
+	}
+}
